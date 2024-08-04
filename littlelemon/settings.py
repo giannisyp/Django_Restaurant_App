1ecb1ba556fcae89e0fb3b8c2ffc060da0558069
Ypaspistis Giannis <giannisypaspistis@gmail.com>
2024-08-04 19:10:34 +0300

    adding whitenoise

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,9 +130,9 @@ STATICFILES_DIRS = [
     "restaurant/static",
 ]
 STORAGES = {
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    }
 }
 
 # Default primary key field type
